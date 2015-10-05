2d79eabaebe4322170aa5427b8deb44c68a72748
nemke <djordjevic.nemanja@gmail.com>
2015-10-05 16:23:33 +0200

    Fixed reading card type. Certificates are not available for new Id cards

---
+++ src/BashChelik/BashChelik/DataModel/CardType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashChelik.DataModel
+{
+    public enum CardType
+    {
+        OldCardAppollo = 1,
+        NewCardGemalto
+    }
+}
