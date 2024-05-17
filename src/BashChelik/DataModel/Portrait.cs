7ebe257094c40384a7b874bbdc250a903f59f054
Nemanja Đorđević <neman@users.noreply.github.com>
2024-05-17 10:49:03 +0200

    Migrate to .NET  8 (#4)

--- src/BashChelik/DataModel/Portrait.cs
+++ src/BashChelik/DataModel/Portrait.cs
@@ -1,4 +1,6 @@
 ﻿using BashChelik.Native;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -11,15 +13,14 @@ namespace BashChelik.DataModel
 {
     public class PortraitData
     {
-        public Bitmap Portrait { get; private set; }
+        public Image<Rgba32> Portrait { get; private set; }
 
         public PortraitData(EID_PORTRAIT nativeData)
         {
             using (var stream = new MemoryStream(nativeData.portrait, 0, nativeData.portraitSize))
-            using (var image = Image.FromStream(stream, false, true))
             {
-                Portrait = new Bitmap(image);
-            }
+                Portrait = Image.Load<Rgba32>(stream);
+            }           
            
         }
     }
